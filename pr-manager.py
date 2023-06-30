47e3021b19219e15eacf9f87ae729ed905c7795c
Nam Pham <nampdn@protonmail.com>
2023-06-30 15:23:48 +0700

    ci(automerge): Update automerge.yml (#5)
    * Update automerge.yml
    
    * Update pr-manager.py
    
    * Create whitelist_authors.txt
    
    * Update automerge.yml

--- pr-manager.py
+++ pr-manager.py
@@ -1,6 +1,6 @@
 from huggingface_hub import HfApi
+from discord_webhook import DiscordWebhook
 import argparse
-
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -8,9 +8,9 @@ def parse_args():
                         help='HuggingFace write token', required=True)
     parser.add_argument('--repo_id', type=str,
                         help='HuggingFace repo_id', required=True)
+    parser.add_argument('--discord_webhook', type=str,
+                        help='Discord Webhook', required=True)
     # allow author: list or str
-    parser.add_argument('--authors', type=str, nargs='+',
-                        help='HuggingFace authors', default=[])
     args = parser.parse_args()
     return args
 
@@ -18,22 +18,61 @@ def parse_args():
 # example run: python .\PR-manager.py --token yourtoken --repo_id Symato/cc --authors khunglong robot ninja
 if __name__ == '__main__':
     args = parse_args()
-    authors = args.authors
+    authors = []
+
+    with open('whitelist_authors.txt', 'r') as f:
+        authors = f.readlines()
+
     if authors:
+        authors = [author.strip() for author in authors]
         print(f"Allow authors: {authors}")
 
     api = HfApi(token=args.token)
 
+    pr_list = []
+
+    print("Loading PR...")
     for discuss in api.get_repo_discussions(repo_id=args.repo_id, repo_type='dataset'):
         if not (discuss.is_pull_request and discuss.status == 'open'):
             continue
         if discuss.author not in authors:
             continue
-        api.merge_pull_request(
-            repo_id='Symato/cc',
-            discussion_num=discuss.num,
-            repo_type='dataset',
-        )
-        print(
-            f"merged {discuss.num}\n{discuss.title}\nAuthor: {discuss.author}")
-        print('='*20)
+        pr_list.append(discuss)
+
+    hf_authors = {}
+    count_by_authors = {}
+    print(f"Merging {len(pr_list)} PR...")
+    for discuss in pr_list:
+        try:
+            if discuss.author in count_by_authors and count_by_authors[discuss.author] >= 5: # Github Actions might timeout, limit per user
+                continue
+            else:
+                # api.merge_pull_request(
+                #     repo_id='Symato/cc',
+                #     discussion_num=discuss.num,
+                #     repo_type='dataset',
+                # )
+                discord_user, processed_file = discuss.title.split(" submit ")
+                if not discuss.author in hf_authors:
+                    hf_authors[discuss.author] = {}
+                    count_by_authors[discuss.author] = 0
+                
+                if not discord_user in hf_authors[discuss.author]:
+                    hf_authors[discuss.author][discord_user] = []
+                
+                count_by_authors[discuss.author] += 1
+
+                hf_authors[discuss.author][discord_user].append(discuss.num)
+        except:
+            a = 0
+    
+    message = ""
+    print(f"Polulating webhook message...")
+    for hf_author, discord_contributors in hf_authors.items():
+        message += f"\n\n### :hugging: https://huggingface.co/{hf_author} đã cùng Symato chung tay góp phần xử lý **{count_by_authors[hf_author]}** CommonCrawl files:\n"
+        sub_msgs = [f"- <@{discord_user}>: {len(pr_nums)} tập tin (PRs: {', '.join([ f'#{pr}' for pr in pr_nums ])})" for discord_user, pr_nums in discord_contributors.items()]
+        message += "\n".join(sub_msgs)
+
+    print(message)
+    webhook = DiscordWebhook(url=args.discord_webhook, content=message)
+    response = webhook.execute()
